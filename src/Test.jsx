94087e0987ce7f5f6e0848ba2b804e6307127de0
Tejasva Jaitly <tejasvajaitly@gmail.com>
2023-05-31 23:10:07 -0500

    added react router dom

---
+++ src/Test.jsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>Hi this is test screen</div>;
+};
+
+export default Test;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import Test from './Test.jsx';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'test',
+    element: <Test />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
