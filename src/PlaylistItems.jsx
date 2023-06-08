de818f995acce8abd8ff23a18cf6f39868f30609
Tejasva Jaitly <tejasvajaitly@gmail.com>
2023-06-08 15:38:28 -0500

    aded spotify links according to spotify guidelines

--- src/PlaylistItems.jsx
+++ src/PlaylistItems.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import {getPlaylistItems} from './utils';
 import Spinner from './Spinner';
+import spotifyLogo from '../public/spotify-icon.png';
 
 const mergerArtistnames = (artistsArray = []) => {
   const artists = artistsArray.map(artist => artist.name);
@@ -39,6 +40,7 @@ const PlaylistItems = () => {
                   name={playlistItem.track?.name}
                   image={playlistItem.track?.album?.images[0]?.url}
                   artists={mergerArtistnames(playlistItem.track?.artists)}
+                  link={playlistItem.track?.external_urls?.spotify}
                 />
               </li>
             ) : null
@@ -51,18 +53,32 @@ const PlaylistItems = () => {
 
 export default PlaylistItems;
 
-const PlaylistItem = ({name, artists = '', image}) => {
+const PlaylistItem = ({name, artists = '', image, link}) => {
   return (
-    <div className="grid grid-cols-[auto,1fr] gap-4 p-2 hover:bg-[hsla(0,0%,100%,.07)] rounded cursor-pointer">
-      <div className="w-12 h-12 rounded">
-        <img src={image} alt={name} className="w-full h-full object-cover rounded" />
-      </div>
-      <div className="flex flex-row justify-start">
-        <div className="flex flex-col">
-          <p className="text-left text-base font-medium text-white">{name}</p>
-          <p className="text-left text-sm text-gray-500">{`${artists}`}</p>
+    <div className="grid grid-cols-[auto,1fr,auto] gap-4 p-2 hover:bg-[hsla(0,0%,100%,.07)] rounded cursor-pointer">
+      <div>
+        <div className="w-12 h-12 rounded">
+          <img src={image} alt={name} className="w-full h-full object-cover rounded" />
         </div>
       </div>
+      <div>
+        <div className="flex flex-row justify-start">
+          <div className="flex flex-col">
+            <p className="text-left text-base font-medium text-white">{name}</p>
+            <p className="text-left text-sm text-gray-500">{`${artists}`}</p>
+          </div>
+        </div>
+      </div>
+
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={link}
+        className="rounded-full px-3 py-1 text-xs flex flex-row justify-around items-center"
+      >
+        <img className="h-3 w-3 mr-2" src={spotifyLogo} />
+        Listen on Spotify
+      </a>
     </div>
   );
 };
